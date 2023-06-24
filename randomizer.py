3aaf296ddb66f9d5952c678bc3f45822b027dd6c
rukky321 <rukkygogo@gmail.com>
2023-06-25 06:45:15 +0900

    レシピ部分を削除

--- randomizer.py
+++ randomizer.py
@@ -1,36 +1,31 @@
 import os
 import random
 import shutil
-import glob
 
-loot_tables_origin_pass = "./default_loot_tables"
-recipes_origin_pass = "./default_recipes"
+# ランダム化前のルートテーブルの保存されている場所のパスを指定
+loot_tables_origin_path = "./default_loot_tables"
 
+# ルートテーブルフォルダ内の全ファイルのパスを取得し、リストにまとめる
 loot_tables = []
-for current_dir, sub_dirs, files_list in os.walk(loot_tables_origin_pass): 
+for current_dir, sub_dirs, files_list in os.walk(loot_tables_origin_path): 
   for file_name in files_list: 
     loot_tables.append(os.path.join(current_dir,file_name))
-recipes = os.listdir(recipes_origin_pass)
-  
-''' os.makedirs("./data/minecraft/recipes", exist_ok=True)
-randomized_recipes = random.sample(recipes,len(recipes))
-for i in range(len(recipes)):
-  origin_pass = recipes_origin_pass+"/"+recipes[i]
-  target_pass = "./data/minecraft/recipes"+"/"+randomized_recipes[i]
-  shutil.copy(origin_pass, target_pass) '''
 
+# データパック内にルートテーブルフォルダを作成
 os.makedirs("./data/minecraft/loot_tables", exist_ok=True)
+# データパック内に入れるルートテーブルファイルのパスのリストを作成する
 target_loot_tables = []
 for i in range(len(loot_tables)):
     target_loot_tables.append("./data/minecraft/loot_tables/"+loot_tables[i].replace("./default_loot_tables/",""))
+# 元のリストのjsonファイルをランダムな順番に取り出し、その内容をデータパック内のファイルに書き込んでいく
 random.shuffle(loot_tables)
 for i in range(len(loot_tables)):
-  origin_pass = loot_tables[i]
-  target_pass = target_loot_tables[i]
-  if target_pass.rfind("/") >= 0:
-    if os.path.isdir(target_pass[0:target_pass.rfind("/")]) != True:
-      os.makedirs(target_pass[0:target_pass.rfind("/")], exist_ok=True)
+  origin_path = loot_tables[i]
+  target_path = target_loot_tables[i]
+  if target_path.rfind("/") >= 0:
+    if os.path.isdir(target_path[0:target_path.rfind("/")]) != True:
+      os.makedirs(target_path[0:target_path.rfind("/")], exist_ok=True)
   try:
-    shutil.copy(origin_pass, target_pass)
-  except IsADirectoryError:
-    print(origin_pass)
+    shutil.copy(origin_path, target_path)
+  except IsADirectoryError as e:
+    print(e)
